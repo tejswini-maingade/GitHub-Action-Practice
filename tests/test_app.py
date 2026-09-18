db4ae88576bc15f273f6997f547b4f978f7b4fd8
Tejswini Maingade <tejswinimaingade@gmail.com>
2026-09-18 19:36:32 +0530

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,7 +4,7 @@ import unittest
 class TestApplication(unittest.TestCase):
 
     def test_addition(self):
-        self.assertEqual(2 + 3, 5)
+        self.assertEqual(2 + 3, 10)
 
     def test_string(self):
         self.assertEqual("DevOps".upper(), "DEVOPS")
